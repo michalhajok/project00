5772509e1b32b766968073f67482ca1b4a96e38c
michalhajok <hajokmichal@gmail.com>
2020-05-22 15:48:56 +0200

    Updates

---
+++ precache-manifest.c145486b2667c0b0de79f083af721d53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87d788537428f90c405a3def0bb135a",
+    "url": "/project00/index.html"
+  },
+  {
+    "revision": "06927b07fb0c918641a2",
+    "url": "/project00/static/css/main.6f62075d.chunk.css"
+  },
+  {
+    "revision": "eeb2fa2c22526100ea44",
+    "url": "/project00/static/js/2.287623cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project00/static/js/2.287623cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06927b07fb0c918641a2",
+    "url": "/project00/static/js/main.246ce66c.chunk.js"
+  },
+  {
+    "revision": "b2142d11fef599b65518",
+    "url": "/project00/static/js/runtime-main.7886227c.js"
+  },
+  {
+    "revision": "23c0a1813e235baf168361e30c67fc29",
+    "url": "/project00/static/media/questionMark.23c0a181.jpg"
+  }
+]);
